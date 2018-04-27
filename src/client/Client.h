5534a35d0fc94cc1cf840685e6a1067d6881b158
Anders Bakken <agbakken@gmail.com>
2018-04-26 19:19:04 -0700

    wip

--- src/client/Client.h
+++ src/client/Client.h
@@ -2,11 +2,13 @@
 #define CLIENT_H
 
 #include <string>
+#include <jeayeson/jeayeson.hpp>
 
 namespace Client {
 std::string findCompiler(int argc, char **argv);
 void parsePath(const char *path, std::string *basename, std::string *dirname);
 [[ noreturn ]] void runLocal(const std::string &compiler, int argc, char **argv);
+
 
 struct WebSocket *wsConnect();
 }
