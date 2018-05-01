0474be3ab1eb93985d5e3844480c0128520ff127
Anders Bakken <agbakken@gmail.com>
2018-04-30 21:39:03 -0700

    Handle output and responses. Untested.

--- client/main.cpp
+++ client/main.cpp
@@ -154,9 +154,47 @@ int main(int argc, char **argv)
         slaveWS.send(WebSocket::Text, json.c_str(), json.size());
         slaveWS.send(WebSocket::Binary, preprocessed->stdOut.c_str(), preprocessed->stdOut.size());
         Watchdog::transition(Watchdog::WaitingForResponse);
-        size_t received = 0;
-        size_t total = std::string::npos;
-        auto process = [&received, &total, &compiler, argc, argv](WebSocket::Mode type, const void *data, size_t len) {
+        struct File {
+            std::string path;
+            size_t remaining;
+        };
+        std::vector<File> files;
+        FILE *f = 0;
+        auto fill = [&files, &f, &compiler, argc, argv](const unsigned char *data, const size_t bytes) {
+            assert(f);
+            auto *front = &files.front();
+            size_t offset = 0;
+            do {
+                const size_t b = std::min(front->remaining, bytes);
+                assert(f);
+                if (b) {
+                    if (fwrite(data + offset, 1, b, f) != b) {
+                        Log::error("Failed to write to file %s (%d %s)", front->path.c_str(), errno, strerror(errno));
+                        Watchdog::stop();
+                        Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
+                        return;
+                    }
+                    offset += b;
+                    front->remaining -= b;
+                }
+                if (!front->remaining) {
+                    fclose(f);
+                    f = 0;
+                    files.erase(files.begin());
+                    if (files.empty())
+                        break;
+                    front = &files.front();
+                    f = fopen(front->path.c_str(), "w");
+                    continue;
+                }
+            } while (offset < bytes);
+            if (offset < bytes) {
+                Log::error("Extraneous bytes. Abandon ship (%zu/%zu)", offset, bytes);
+                Watchdog::stop();
+                Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
+            }
+        };
+        auto process = [&compiler, argc, argv, &files, &f, &fill](WebSocket::Mode type, const void *data, size_t len) {
             if (type == WebSocket::Text) {
                 Log::debug("GOT MSG [%s]", std::string(reinterpret_cast<const char *>(data), len).c_str());
                 std::string err;
@@ -167,18 +205,63 @@ int main(int argc, char **argv)
                     Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
                     return;
                 }
-                // total =
+
+                const std::string type = msg["type"].string_value();
+
+                if (type == "output") {
+                    const std::string output = msg["output"].string_value();
+                    if (!output.empty()) {
+                        fwrite(output.c_str(), 1, output.size(), msg["stderr"].bool_value() ? stderr : stdout);
+                    }
+                    return;
+                }
+
+                if (type != "response") {
+                    Log::error("Unexpected message type %s. Wanted \"response\"", msg["type"].string_value().c_str());
+                    Watchdog::stop();
+                    Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
+                    return;
+                }
+                json11::Json::array index = msg["index"].array_items();
+                if (index.empty()) {
+                    Log::error("No files?");
+                    Watchdog::stop();
+                    Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
+                    return;
+                }
+                files.resize(index.size());
+                for (size_t i=0; i<index.size(); ++i) {
+                    File &ff = files[i];
+                    ff.path = index[i]["path"].string_value();
+                    ff.remaining = index[i]["bytes"].int_value();
+                    if (ff.path.empty()) {
+                        Log::error("No file for idx: %zu", i);
+                        Watchdog::stop();
+                        Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
+                        return;
+                    }
+                }
+                f = fopen(files[0].path.c_str(), "w");
+                if (files[0].remaining)
+                    fill(0, 0);
             } else {
                 Log::debug("Got binary data: %zu bytes", len);
-
-            }
+                if (files.empty()) {
+                    Log::error("Unexpected binary data (%zu bytes)", len);
+                    Watchdog::stop();
+                    Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
+                    return;
+                }
+                fill(reinterpret_cast<const unsigned char *>(data), len);
+            };
         };
-
-        while (received < total) {
+        do {
             if (!slaveWS.process(process)) {
-                break;
+                Log::debug("Have to run locally because failed to get message from slave");
+                Watchdog::stop();
+                return Client::runLocal(compiler, argc, argv, Client::acquireSlot(Client::Wait));
             }
-        }
+        } while (!files.empty());
     } else {
         Log::debug("Have to run locally because no slave");
         Watchdog::stop();
