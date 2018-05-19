fccbd11469c91d64f8070e129fe01525f5a254ee
Jan Erik Hanssen <jhanssen@gmail.com>
2018-05-18 22:35:17 -0700

    test chart

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { FiskService } from './fisk.service';
 
 @Component({
@@ -8,14 +9,33 @@ import { FiskService } from './fisk.service';
 })
 export class AppComponent {
     title = 'app';
+    data: any = undefined;
+
+    view: any[] = [700, 400];
+    colorScheme = {
+        domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    };
+    showXAxis = true;
+    showYAxis = true;
+    gradient = false;
+    showLegend = true;
+    showXAxisLabel = true;
+    xAxisLabel = 'Country';
+    showYAxisLabel = true;
+    yAxisLabel = 'Population';
 
     constructor(private fisk: FiskService) {
         fisk.open("localhost", 8999);
         fisk.on("data", (data: any) => {
             console.log("got data from fisk", data);
+            this.data = data;
         });
     }
 
     connect(host: string, port: number) {
+    }
+
+    onSelect(event) {
+        console.log(event);
     }
 }
