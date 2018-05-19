d8feb21e0370148f5eab23b85d555d96a3b5bbba
Jan Erik Hanssen <jhanssen@gmail.com>
2018-05-18 22:29:21 -0700

    even more

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FiskService } from './fisk.service';
-import { BackoffService } from './backoff.service';
 
 @Component({
     selector: 'app-root',
@@ -10,48 +9,13 @@ import { BackoffService } from './backoff.service';
 export class AppComponent {
     title = 'app';
 
-    private pendingConnect: Array<any> = [];
-
-    constructor(private fisk: FiskService, private backoff: BackoffService) {
-        this.connect("localhost", 8999);
+    constructor(private fisk: FiskService) {
+        fisk.open("localhost", 8999);
+        fisk.on("data", (data: any) => {
+            console.log("got data from fisk", data);
+        });
     }
 
     connect(host: string, port: number) {
-        this.fisk.on("message", (data: any) => {
-            console.log("hey", data);
-            this.fisk.send({ ting: "tang" });
-        });
-        this.fisk.on("close", () => {
-            // let's retry with an exponential backoff
-            if (this.backoff.running("app")) {
-                this.resolvePending(false);
-                return;
-            }
-            const when = (next: number): number => {
-                if (!next)
-                    return 1000;
-                return Math.min(30000, next * 2);
-            };
-            this.backoff.backoff("app", when, (): Promise<any> => {
-                return new Promise<any>((resolve, reject) => {
-                    this.pendingConnect.push({ resolve: resolve, reject: reject });
-                    this.connect("localhost", 8999);
-                });
-            });
-        });
-        this.fisk.on("open", () => {
-            this.resolvePending(true);
-            console.log("ok");
-        });
-        this.fisk.on("error", () => {
-        });
-        this.fisk.open("localhost", 8999);
-    }
-
-    private resolvePending(ok: boolean) {
-        if (this.pendingConnect.length > 0) {
-            const pending = this.pendingConnect.shift();
-            pending.resolve(ok);
-        }
     }
 }
---
+++ ui/src/app/websocket.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class WebSocketService {
+    private socket: WebSocket;
+    private pending: Array<string>;
+    private messageListeners: { (data: any): void; } [];
+    private errorListeners: { (data: any): void; } [];
+    private closeListeners: { (): void; } [];
+    private openListeners: { (): void; } [];
+    private isopen: boolean;
+
+    constructor() {
+        this.reset();
+    }
+
+    open(host: string, port: number) {
+        if (this.isopen) {
+            return;
+        }
+        this.socket = new WebSocket(`ws://${host}:${port}`);
+
+        this.socket.addEventListener('open', event => {
+            this.isopen = true;
+            // send all the pending stuff
+            for (let i = 0; i < this.pending.length; ++i) {
+                this.socket.send(this.pending[i]);
+            }
+            this.pending = undefined;
+
+            for (let i = 0; i < this.openListeners.length; ++i) {
+                this.openListeners[i]();
+            }
+        });
+        this.socket.addEventListener('message', event => {
+            let data: any;
+            try {
+                data = JSON.parse(event.data);
+            } catch (e) {
+                console.error("unable to parse json", event.data);
+                return;
+            }
+            for (let i = 0; i < this.messageListeners.length; ++i) {
+                this.messageListeners[i](data);
+            }
+        });
+        this.socket.addEventListener('close', () => {
+            for (let i = 0; i < this.closeListeners.length; ++i) {
+                this.closeListeners[i]();
+            }
+            this.reset();
+        });
+        this.socket.addEventListener('error', (err) => {
+            for (let i = 0; i < this.errorListeners.length; ++i) {
+                this.errorListeners[i](err);
+            }
+        });
+    }
+
+    on(name: string, on: { (data?: any): void; }) {
+        if (name == "message") {
+            this.messageListeners.push(on);
+        } else if (name == "open") {
+            this.openListeners.push(on);
+        } else if (name == "close") {
+            this.closeListeners.push(on);
+        } else if (name == "error") {
+            this.errorListeners.push(on);
+        }
+    }
+
+    send(data: any) {
+        if (this.isopen) {
+            this.socket.send(JSON.stringify(data));
+        } else {
+            this.pending.push(JSON.stringify(data));
+        }
+    }
+
+    private reset() {
+        this.socket = undefined;
+        this.pending = [];
+        this.messageListeners = [];
+        this.closeListeners = [];
+        this.errorListeners = [];
+        this.openListeners = [];
+        this.isopen = false;
+    }
+}
