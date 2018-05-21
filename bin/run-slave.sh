1dafd23e5ace9edbe0b87449af338e6f9040acc3
Anders Bakken <agbakken@gmail.com>
2018-05-21 11:36:07 -0700

    bump again.

--- bin/run-slave.sh
+++ bin/run-slave.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../slave
 while true; do
-    npm install --unsafe-perm -g dcfisk
-    node ./index.js
+    npm install --unsafe-perm -g dcfisk > log
+    node ./index.js >> log
 done
 
