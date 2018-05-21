fc43e371adb2425ee88d715e61d52c507ab6f968
Anders Bakken <agbakken@gmail.com>
2018-05-21 12:29:29 -0700

    gooder

--- bin/run-slave.sh
+++ bin/run-slave.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../slave
 while true; do
-    npm install --unsafe-perm -g dcfisk > log
-    node ./index.js >> log
+    npm install --unsafe-perm -g dcfisk > /var/log/dcfisk-slave.log
+    cd "$PWD"
+    node ./index.js >> /var/log/dcfisk-slave.log
 done
 
