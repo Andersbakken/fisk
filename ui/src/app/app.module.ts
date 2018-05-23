bc315c44bcbcd52960984489cc411c3f9e7d10d1
Jan Erik Hanssen <jhanssen@gmail.com>
2018-05-22 20:32:18 -0700

    hey

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,15 @@ import { WebSocketService } from './websocket.service';
 import { BackoffService } from './backoff.service';
 import { FiskService } from './fisk.service';
 import { ConfigComponent } from './config/config.component';
+
+const appRoutes: Routes = [
+    { path: 'config', component: ConfigComponent },
+    { path: 'chart', component: ChartComponent },
+    { path: '',
+      redirectTo: '/chart',
+      pathMatch: 'full'
+    }
+];
 
 @NgModule({
     declarations: [
@@ -19,6 +29,7 @@ import { ConfigComponent } from './config/config.component';
         BrowserModule,
         BrowserAnimationsModule,
         NgxChartsModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [WebSocketService, BackoffService, FiskService],
     bootstrap: [AppComponent]
