0c012bf3dcd7c970f8aaa7f9b8fedc9a36f41e16
Anders Bakken <agbakken@gmail.com>
2018-05-29 15:28:10 -0700

    oops.

--- bin/run-slave.sh
+++ bin/run-slave.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../slave
 while true; do
-    npm install --unsafe-perm -g fisk &> /var/log/fisk-slave.log
+    npm install --unsafe-perm -g @andersbakken/fisk &> /var/log/fisk-slave.log
     cd "$PWD"
     node ./index.js &>> /var/log/fisk-slave.log
 done
