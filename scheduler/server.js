d7b43360f8d9a13945bcf06a575b5b9b784475d7
Anders Bakken <agbakken@gmail.com>
2018-06-06 14:10:40 -0700

    this is not an int

--- scheduler/server.js
+++ scheduler/server.js
@@ -133,7 +133,7 @@ class Server extends EventEmitter {
             const hostname = req.headers["x-fisk-slave-hostname"];
             const system = req.headers["x-fisk-system"];
             const slots = parseInt(req.headers["x-fisk-slots"]);
-            const version = parseInt(req.headers["x-fisk-npm-version"]);
+            const version = req.headers["x-fisk-npm-version"];
             // console.log(req.headers);
             let environments = {};
             req.headers["x-fisk-environments"].replace(/\s+/g, '').split(';').forEach(env => {
