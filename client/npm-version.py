fd433ac897c58c37f75f552ee2bb1e1997e4eeea
Anders Bakken <agbakken@gmail.com>
2021-08-31 16:40:42 -0700

    Use bash instead.

--- client/npm-version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import json
-import os
-import sys
-
-with open(os.path.realpath(os.path.dirname(__file__)) + '/../package.json') as f:
-    data = json.load(f)
-    print data["version"];
