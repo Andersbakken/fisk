496c80e1fce8dc9b01d5a1ca0aea52429b44e7e4
Anders Bakken <agbakken@gmail.com>
2021-09-01 16:45:05 -0700

    This hasn't been here since forever.

--- client/create-npm-version.cmake
+++ client/create-npm-version.cmake
@@ -4,7 +4,6 @@ execute_process(COMMAND bash ${CMAKE_CURRENT_LIST_DIR}/npm-version.sh
     OUTPUT_VARIABLE VERSION
     ERROR_VARIABLE ERROR
     RESULT_VARIABLE RESULT
-    ECHO_OUTPUT_VARIABLE
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 message(STATUS "NPM Version ${VERSION}")
 file(APPEND ${OUTPUT} "const char *npm_version = \"${VERSION}\";\n")
