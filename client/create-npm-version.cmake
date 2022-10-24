617d6252a90d78625c2647d335ef2728f314a14e
Anders Bakken <agbakken@gmail.com>
2022-10-23 18:34:18 -0700

    Update min version.

--- client/create-npm-version.cmake
+++ client/create-npm-version.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 2.8)
+cmake_minimum_required(VERSION 3.0)
 file(WRITE ${OUTPUT} "")
 execute_process(COMMAND bash ${CMAKE_CURRENT_LIST_DIR}/npm-version.sh
     OUTPUT_VARIABLE VERSION
