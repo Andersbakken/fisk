e17c401ded3596cda718308c925d53ab6b6252f3
Anders Bakken <agbakken@gmail.com>
2023-01-24 17:05:39 -0800

    typescript build system.

---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import hashbang from "rollup-plugin-hashbang";
+
+const plugins = [
+    resolve({
+        preferBuiltins: true
+    }),
+    commonjs(),
+    typescript({
+        tsconfig: `tsconfig.json`,
+        cacheRoot: ".cache"
+    }),
+    hashbang()
+];
+
+// Define forms
+const format = "cjs";
+const external = ["fs", "assert"];
+
+export default [
+    {
+        input: "daemon/fisk-daemon.ts",
+        plugins,
+        external,
+        output: {
+            file: "dist/fisk-daemon.js",
+            format,
+            name: "fisk-daemon",
+            exports: "named",
+            sourcemap: true
+        }
+    },
+    {
+        input: "builder/fisk-builder.ts",
+        plugins,
+        external,
+        output: {
+            file: "dist/fisk-builder.js",
+            format,
+            name: "fisk-builder",
+            exports: "named",
+            sourcemap: true
+        }
+    },
+    {
+        input: "builder/VM_runtime.ts",
+        plugins,
+        external,
+        output: {
+            file: "dist/VM_runtime.js",
+            format,
+            name: "fisk-builder-VM_runtime",
+            exports: "named",
+            sourcemap: true
+        }
+    }
+
+];
