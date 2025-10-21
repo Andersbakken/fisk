0a91fa9d8466cece8c481c1cc5d856e3d4eba743
Anders Bakken <agbakken@gmail.com>
2025-10-21 16:06:17 -0700

    Validate cache version

--- src/common/index.ts
+++ src/common/index.ts
@@ -3,7 +3,8 @@ import os from "os";
 import path from "path";
 import type { Options } from "@jhanssen/options";
 
-const Version = 6;
+const Version = 5;
+const ObjectCacheFormatVersion = 1;
 
 function cacheDir(option: Options): string {
     let dir = option("cache-dir");
@@ -36,15 +37,41 @@ function validateCache(option: Options): void {
     fs.writeFileSync(file, buf);
 }
 
+function validateObjectCache(option: Options): void {
+    const dir = cacheDir(option);
+    const objectCacheDir = option.string("object-cache-dir") || path.join(dir, "objectcache");
+    const file = path.join(objectCacheDir, "version");
+    let version;
+    try {
+        version = fs.readFileSync(file);
+        if (version.readUInt32BE() === ObjectCacheFormatVersion) {
+            return;
+        }
+    } catch (err) {
+        /* */
+    }
+    if (version) {
+        console.log(`Wrong object cache version. Destroying object cache ${objectCacheDir}`);
+    }
+    fs.removeSync(objectCacheDir);
+    fs.mkdirpSync(objectCacheDir);
+    const buf = Buffer.allocUnsafe(4);
+    buf.writeUInt32BE(ObjectCacheFormatVersion);
+    fs.writeFileSync(file, buf);
+}
+
 export interface Common {
     cacheDir: () => string;
     Version: number;
+    ObjectCacheFormatVersion: number;
 }
 
 export function common(option: Options): Common {
     validateCache(option);
+    validateObjectCache(option);
     return {
         cacheDir: cacheDir.bind(undefined, option),
-        Version
+        Version,
+        ObjectCacheFormatVersion
     };
 }
